8c3c29f95dceac61f1fc81b2e835336d412b4d8c
andrey <dyhazykov@yandex.ru>
2026-09-06 14:36:16 +0300

    Tomporarily disable JPA

--- src/main/java/org/booking/bookingagency/BookingAgencyApplication.java
+++ src/main/java/org/booking/bookingagency/BookingAgencyApplication.java
@@ -2,8 +2,9 @@ package org.booking.bookingagency;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class BookingAgencyApplication {
 
     public static void main(String[] args) {
